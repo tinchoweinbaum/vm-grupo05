efa05dee9b895a5442e2c7b39d0276c911e22b4c
Andoni Catalano <andonicatalano1@gmail.com>
2025-09-11 15:52:21 -0300

    Add files via upload

--- operations.c
+++ operations.c
@@ -1,80 +1,45 @@
-//habría que incluír la funcion NZ del main.c en operations.c para que las instrucciones
-//justamente afecten a NZ
 
+void MOV(mv *MV, char topA, char topB){
+    int aux;
 
-//faltan las instrucciones de logica de bits: shifts y puertas logicas
+    aux = get_op(MV,topB);
 
-#include "operations.h"
-
-void MOV(char opB, char *opA){
-    *opA = opB;
-}
-
-void ADD(char opB, char *opA){
-    *opA += opB;
-}
-
-
-void SUB(char opB, char *opA){
-    *opA -= opB;
-}
-
-void MUL(char opB, char *opA){
-    *opA = (*opA) * opB;
-}
-
-void DIV(char opB, char *opA, int *error, char *ac){
-    if(opB!=0){
-        (*opA)/=opB;
-        *ac= (*opA) % opB;
+    if (topA == 0b01)
+        MV->reg[OP1] = aux;
+    else {
+        writemem(MV);
     }
+}
+
+void ADD(mv *MV, char topA, char topB){
+    int aux;
+
+    aux = get_op(MV,topB);
+
+    if (topA == 0b01)
+        MV->reg[OP1] += aux;
+    else {
+        writemem(MV);
+    }
+}
+void MUL(mv *MV, char topA, char topB){
+    int aux;
+
+    aux = get_op(MV,topB);
+
+    if (topA == 0b01)
+        MV->reg[OP1] *= aux;
+    else {
+        writemem(MV);
+    }
+}
+void SUB(mv *MV, char topA, char topB){
+    int aux;
+
+    aux = get_op(MV,topB);
+
+    if (topA == 0b01)
+        MV->reg[OP1] -= aux;
     else
-        *error = 1; //hacer los códigos de errores por valores de la variable
-}
-
-void CMP(char opB, char opA){
-    int res = opA - opB;
-}
-
-void SHL(char opB, char *opA){
-
-}
-
-void SHR(char opB, char *opA){
-    
-}
-
-void SAR(char opB, char *opA){
-    
-}
-
-void AND(char opB, char *opA){
-    (*opA) = (*opA) & opB;
-}
-
-void OR(char opB, char *opA){
-    (*opA) = (*opA) | opB;
-}
-
-void XOR(char opB, char *opA){
-
-}
-
-void SWAP(char *opB, char *opA){
-    (*opA)=(*opB);
-}
-
-void LDL(char opB, char *opA){
-
-}
-
-void LDL(char opB, char *opA){
-
-}
-
-void RND(char opB, char *opA){
-    if(opB>0){
-        int random = rand() % opB + 1;
-        *opA = random;
-    }
+        writemem(MV);
 }
