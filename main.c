d7efcdcb31aa6a3a0bef5106528a25bff17352db
Andoni Catalano <andonicatalano1@gmail.com>
2025-09-11 15:17:03 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include "operations.h"
 
+/**********REGISTROS***********/
+
+#define LAR 0
+#define MAR 1
+#define MBR 2
+
+#define IP 3
+#define OPC 4
+#define OP1 5
+#define OP2 6
+
+#define EAX 10
+#define EBX 11
+#define ECX 12
+#define EDX 13
+#define EEX 14
+#define EFX 15
+
+#define AC 16
+#define CC 17
+
+#define CS 26
+#define DS 27
+
+/***************TAMANIOS**************/
+
 #define MEM_SIZE 16384 //16384 bytes == 16 KiB
 #define REG_SIZE 32 //32 registros en el procesador de la VM.
 #define HEADER_SIZE 7 //el encabezado ocupa del byte 0 al 7 de un archivo
